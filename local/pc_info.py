b9599e96618afed9fc5b6e8f29832a81f8178a54
ming <18120580001@163.com>
2020-01-02 17:24:24 +0800

    feat (local): 增加local功能
    update pc-info 使用shell获取相关配置

--- local/pc_info.py
+++ local/pc_info.py
@@ -13,34 +13,35 @@ def echo_pc_info():
     """
     virtual_mem = psutil.virtual_memory()
     swap_mem = psutil.swap_memory()
-    osinfo = os.uname()
+    os_info = os.uname()
 
     memory_info_str = '''
-    操作系统:
-        主机名:     {}
-        发行版本:   {}
-        内核版本:   {}
-        硬件架构:   {}
-    cpu信息:
-        cpu型号:    {}
-        cpu物理核心: {}
-        cpu逻辑核心: {}
-    内存信息:
-        物理内存: {}GB 
-        虚拟内存: {}GB 
-    磁盘信息:
-        磁盘大小: {}GB
+操作系统:
+  主机名:     {}
+  发行版本:   {}
+  内核版本:   {}
+  硬件架构:   {}
+  
+cpu信息:
+  cpu型号:    {}
+  cpu物理核心: {}
+  cpu逻辑核心: {}
+  
+内存信息:
+{}
+
+磁盘信息:
+{}
         '''.format(
-        osinfo.nodename,
+        os_info.nodename,
         linux_issue(),
-        osinfo.release,
-        osinfo.machine,
+        os_info.release,
+        os_info.machine,
         cpu_info().modelName,
         psutil.cpu_count(logical=False),
         psutil.cpu_count(),
-        byteToGb(virtual_mem.total),
-        byteToGb(swap_mem.total),
-        psutil.disk_usage('/')
+        subprocess.getoutput('free -h'),
+        subprocess.getoutput('df -h')
     )
     click.echo(memory_info_str)
 
