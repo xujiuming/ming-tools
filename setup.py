d5db0370f79c37a6568eddb4ac7652daf686dcf7
ming <18120580001@163.com>
2020-01-08 11:43:11 +0800

    chore (all): 调整依赖
    update 固定依赖版本

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='jiuming-tools',
     # 版本 如果需要发布更新 需要调整版本号
-    version='1.10',
+    version='1.12',
     packages=find_packages(),
     include_package_data=True,
-
     # 需要的依赖
     install_requires=[
-        'Click', 'psutil', 'PyYAML', 'paramiko'
+        'Click==7.0', 'psutil==5.6.7', 'PyYAML==5.2', 'paramiko==2.7.1'
     ],
     # 命令行入口
     entry_points='''
