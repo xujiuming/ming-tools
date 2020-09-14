f06937726938c4b205db3f918cc43bc41a72a457
ming <18120580001@163.com>
2020-09-14 16:09:13 +0800

    feat (更新版本): 发布1.49版本
    add 当无法获取虚拟化方式的时候 显示无

---
+++ LeetCode/初级算法/删除排序数组中的重复项.py
@@ -0,0 +1,19 @@
+from typing import List
+
+'''
+设定 下标 index 
+从第二个元素开始循环 
+当元素的值 跟 index位置的值不相等 则证明是新的值  
+设置 index+1位置的值为 当前i的值 
+# 截断数组 保留 0-index+1
+'''
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        index = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[index]:
+                index += 1
+                nums[index] = nums[i]
+        return index+1
+
