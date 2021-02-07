2f21a6493215649df466d96632751ee84d5a8179
ming <18120580001@163.com>
2021-02-07 15:02:16 +0800

    add initgit.sh

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# author ming
+# set git global config
+git config --global user.email "18120580001@163.com"
+git config --global user.name "ming"
+git config --global core.editor "vim"
